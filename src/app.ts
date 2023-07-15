f5df99b63a4bb75ccab3c6f06165070f549cf00b
Fernando Brino <fernandobrino11@gmail.com>
2023-07-15 19:34:55 -0300

    feat: integrate with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Fernando Brino",
+    email: "fernandobrino11@gmail.com",
+  },
+});
