5ccaf74d709c204756b1819b01a61c5ddd8d3023
Fernando Brino <fernandobrino11@gmail.com>
2023-07-19 16:22:19 -0300

    feat: create user route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post("/users", async (request, reply) => {
+  const registeerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-prisma.user.create({
-  data: {
-    name: "Fernando Brino",
-    email: "fernandobrino11@gmail.com",
-  },
+  const { name, email, password } = registeerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
 });
