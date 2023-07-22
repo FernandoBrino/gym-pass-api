98028582a1b79c3ffe2a16fcefe36131a0f0cfe5
Fernando Brino <fernandobrino11@gmail.com>
2023-07-22 20:45:36 -0300

    feat: authenticate controller

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -5,13 +5,13 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-  const registeerBodySchema = z.object({
+  const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
   });
 
-  const { name, email, password } = registeerBodySchema.parse(request.body);
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
     const usersRepository = new PrismaUsersRepository();
