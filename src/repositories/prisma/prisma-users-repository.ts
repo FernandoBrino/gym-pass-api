8d09f55d1d07cf912dd101855a0af0da83626e08
Fernando Brino <fernandobrino11@gmail.com>
2023-07-23 12:14:33 -0300

    feat: get user profile service

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,12 @@
 import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
+import { GetResult } from "@prisma/client/runtime/library";
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string) {
+    return;
+  }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
