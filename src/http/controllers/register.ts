f523ea301b54de823ffe91277a433ab1d2d4fa18
Fernando Brino <fernandobrino11@gmail.com>
2023-07-23 11:52:09 -0300

    refactor: using Factory Pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "@/services/errors/user-already-exists-error";
+import { makeRegisterService } from "@/services/factories/make-register-service";
 import { RegisterService } from "@/services/register";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -14,9 +15,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerService = new RegisterService(usersRepository);
-
+    const registerService = makeRegisterService();
     await registerService.execute({ name, email, password });
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
