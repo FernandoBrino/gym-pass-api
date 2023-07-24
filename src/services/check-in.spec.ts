c4c2ec968338e5b6e78d2acfdb93ef9e7a5f09a2
Fernando Brino <fernandobrino11@gmail.com>
2023-07-24 14:52:35 -0300

    test:  create check-in

---
+++ src/services/check-in.spec.ts
@@ -0,0 +1,23 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { CheckInService } from "./check-in";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: CheckInService;
+
+describe("Get User Profile Service", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new CheckInService(checkInsRepository);
+  });
+
+  it("should be able to check in", async () => {
+    // sut -> pattern called system under test, its the main entity that is being tested;
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
