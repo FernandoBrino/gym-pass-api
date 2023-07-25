a15beac81266cbda42aecb8eff47dc4f38d91291
Fernando Brino <fernandobrino11@gmail.com>
2023-07-25 15:11:17 -0300

    feat: validate distance between gym and user

--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { GymsRepository } from "@/repositories/gyms-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-ccordinates";
 
 interface CheckInServiceRequest {
   userId: string;
@@ -23,6 +24,8 @@ export class CheckInService {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
     const gym = await this.gymsRepository.findById(gymId);
 
@@ -31,6 +34,22 @@ export class CheckInService {
     }
 
     // calculate distance between user and gym
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      }
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1; // 0.1 = 100 meters
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error();
+    }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
