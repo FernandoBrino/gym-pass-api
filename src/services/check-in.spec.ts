e26ca54cb861571ddc3930f58a6a7be42972a1cf
Fernando Brino <fernandobrino11@gmail.com>
2023-07-25 11:31:28 -0300

    test: added user latitude and longitude parameters

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -1,14 +1,28 @@
 import { expect, describe, it, beforeEach, afterEach, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInService } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gym-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInService;
 
 describe("Get User Profile Service", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInService(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInService(checkInsRepository, gymsRepository);
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "JavaScript Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
+
     vi.useFakeTimers();
   });
 
@@ -20,6 +34,8 @@ describe("Get User Profile Service", () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: -23.9675882,
+      userLongitude: -46.3912777,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
@@ -31,29 +47,39 @@ describe("Get User Profile Service", () => {
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: -23.9675882,
+      userLongitude: -46.3912777,
     });
 
     await expect(() =>
       sut.execute({
         gymId: "gym-01",
         userId: "user-01",
+        userLatitude: -23.9675882,
+        userLongitude: -46.3912777,
       })
     ).rejects.toBeInstanceOf(Error);
   });
 
   it("should be able to check in twice but in different days", async () => {
+    // set a date
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
 
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: -23.9675882,
+      userLongitude: -46.3912777,
     });
 
+    // change the date and create another check-in
     vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
 
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: -23.9675882,
+      userLongitude: -46.3912777,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
