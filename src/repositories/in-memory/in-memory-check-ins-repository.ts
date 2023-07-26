2eb7b610ea160d1e6b2a369cebde73cc04f358d9
Fernando Brino <fernandobrino11@gmail.com>
2023-07-26 14:27:42 -0300

    feat: get user metrics

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -2,10 +2,13 @@ import { randomUUID } from "node:crypto";
 import { CheckInsRepository } from "../check-ins-repository";
 import { CheckIn, Prisma } from "@prisma/client";
 import dayjs from "dayjs";
-import { GetResult } from "@prisma/client/runtime/library";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
 
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date"); // return the start of the day H=00, M=00, S=00
