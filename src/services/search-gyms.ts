8ac46faa0afc17c16378b47dfa95ccefcdfc5531
Fernando Brino <fernandobrino11@gmail.com>
2023-07-26 19:59:42 -0300

    feat: Search gym service

---
+++ src/services/search-gyms.ts
@@ -0,0 +1,28 @@
+import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { Gym } from "@prisma/client";
+import { GymsRepository } from "@/repositories/gyms-repository";
+
+interface SearchGymsServiceRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymsServiceResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsService {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsServiceRequest): Promise<SearchGymsServiceResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page);
+
+    return {
+      gyms,
+    };
+  }
+}
