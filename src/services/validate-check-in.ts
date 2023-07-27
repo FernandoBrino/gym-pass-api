3347bacd96685579c706e58edb3d56d9de286b70
Fernando Brino <fernandobrino11@gmail.com>
2023-07-27 11:30:06 -0300

    feat: validate check-in service

---
+++ src/services/validate-check-in.ts
@@ -0,0 +1,37 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-ccordinates";
+import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
+import { MaxDistanceError } from "./errors/max-distance-error";
+
+interface ValidateCheckInServiceRequest {
+  checkInId: string;
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn;
+}
+
+export class ValidateCheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId);
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError();
+    }
+
+    checkIn.validated_at = new Date();
+
+    await this.checkInsRepository.save(checkIn);
+
+    return {
+      checkIn,
+    };
+  }
+}
