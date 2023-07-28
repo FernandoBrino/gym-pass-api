778d920011d62fe0c00dc323b2298ffeedfb8359
Fernando Brino <fernandobrino11@gmail.com>
2023-07-27 21:38:18 -0300

    feat: create prisma check in repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,11 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
-import { GetResult } from "@prisma/client/runtime/library";
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(id: string) {
-    return;
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
   }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
