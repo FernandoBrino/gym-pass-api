c00c86f15f83df54cb610fdc0e2e2eeedbb74c38
Fernando Brino <fernandobrino11@gmail.com>
2023-07-31 11:51:47 -0300

    test: create gym e2e

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to creat a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Javascript Gym",
+        description: "some description",
+        phone: "13981184466",
+        latitude: -23.9675882,
+        longitude: -46.3912777,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
