3af4c484776d379499b067e0783c5c115a1e28e9
Fernando Brino <fernandobrino11@gmail.com>
2023-07-31 20:31:51 -0300

    feat: refresh token controller

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,41 @@
+import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error";
+import { makeAuthenticateService } from "@/services/factories/make-authenticate-service";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({
+    onlyCookie: true,
+  }); // verify if refresh token exists
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    }
+  );
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: "7d",
+      },
+    }
+  );
+
+  return reply
+    .setCookie("refreshToken", refreshToken, {
+      path: "/", // back-end route that can access the cookie
+      secure: true, // isn't possible to read the cookie
+      sameSite: true, // just can be accessible inside our only domain
+      httpOnly: true, // just our back-end can access the cookie, not the browser
+    })
+    .status(200)
+    .send({
+      token,
+    });
+}
